d767d7966d4fb4e03dfac688a0b6c20f6deb66a2
athompson-git <a.thompson@northwestern.edu>
2024-10-14 11:48:59 -0500

    Adding 1g1e data

---
+++ data/1g1e0p/parse_ttree.C
@@ -0,0 +1,61 @@
+void parse_ttree() {
+    const char* filename = "analysis_1g1em_run_4_in_numubar_flux_numubar_1M_events.root";
+    const char* tree_name = "gtree_flat";
+
+    TFile *file = new TFile(filename, "READ");
+    TTree *tree = (TTree*)file->Get(tree_name);
+    
+    TBranch *p1em = tree->GetBranch("pxe");
+    TBranch *p2em = tree->GetBranch("pye");
+    TBranch *p3em = tree->GetBranch("pze");
+    TBranch *p0em = tree->GetBranch("ee");
+
+    TBranch *p1ep = tree->GetBranch("pxgamma");
+    TBranch *p2ep = tree->GetBranch("pygamma");
+    TBranch *p3ep = tree->GetBranch("pzgamma");
+    TBranch *p0ep = tree->GetBranch("egamma");
+
+    
+    // Prepare the branch data variable and set the branch address
+    float p1_electron;
+    float p2_electron;
+    float p3_electron;
+    float p0_electron;
+    
+    float p1_positron;
+    float p2_positron;
+    float p3_positron;
+    float p0_positron;
+
+    p1em->SetAddress(&p1_electron);
+    p2em->SetAddress(&p2_electron);
+    p3em->SetAddress(&p3_electron);
+    p0em->SetAddress(&p0_electron);
+    p1ep->SetAddress(&p1_positron);
+    p2ep->SetAddress(&p2_positron);
+    p3ep->SetAddress(&p3_positron);
+    p0ep->SetAddress(&p0_positron);
+
+
+    std::ofstream flux;
+    flux.open("emgamma_numubar_4vectors_DUNE_bkg.txt");
+
+    // Loop over the entries and print the branch data for each entry
+    for (Long64_t entry = 0; entry < tree->GetEntries(); ++entry) {
+        tree->GetEntry(entry);
+        std::cout << "Entry " << entry << ": " << p1_electron << std::endl;
+	flux << p0_electron << " " << p1_electron << " " \
+		<< p2_electron << " " << p3_electron << " " << \
+		p0_positron << " " << p1_positron << " " << \
+		p2_positron << " " << p3_positron << std::endl;
+    }
+
+    flux.close();
+    file->Close();
+    delete file;
+}
+
+//int main() {
+//    parse_ttree();
+//    return 0;
+//}
